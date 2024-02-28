25da6053d9cb0ddc981ee917cf34f8b71e516fa8
DerekMarmol <134981434+DerekMarmol@users.noreply.github.com>
2024-02-28 11:02:01 -0600

    Add files via upload

---
+++ Desafio1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("!Bienvenido¡, ingrese su nombre de usuario");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("A continuación debe ingresar sus ingresos totales de los ultimos 3 mese, empezando por el primer mes");
+            int ingreso1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite su segundo ingreso");
+            int ingreso2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite su tercer ingreso");
+            int ingreso3 = Convert.ToInt32(Console.ReadLine());
+
+            int suma = ingreso1 + ingreso2 + ingreso3;
+            int promedio = suma / 3;
+
+            Console.WriteLine("Hola {0}, en total ganaste {1:C} y promediaste {2:C}.", nombre, suma, promedio);
+            Console.Read();
+        }
+
+    }
+}
---
+++ Desafio3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            int resultado = SumarNumeros();
+            Console.WriteLine("La suma es: " + resultado);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error inesperado: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Operación completada.");
+        }
+
+        // Espera a que el usuario presione Enter para salir
+        Console.ReadLine();
+    }
+
+    public static int SumarNumeros()
+    {
+        Console.WriteLine("Ingrese el primer número:");
+        string input1 = Console.ReadLine();
+        Console.WriteLine("Ingrese el segundo número:");
+        string input2 = Console.ReadLine();
+
+        // Convertir los valores ingresados a enteros
+        int num1 = Convert.ToInt32(input1);
+        int num2 = Convert.ToInt32(input2);
+
+        // Realizar la suma
+        int suma = num1 + num2;
+        return suma;
+    }
+}
