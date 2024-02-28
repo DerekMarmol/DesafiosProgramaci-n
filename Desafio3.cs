c536b87a0cf2a8254b3e38f3da2cb9dcb56bebc6
DerekMarmol <134981434+DerekMarmol@users.noreply.github.com>
2024-02-28 11:02:45 -0600

    Update Desafio3.cs

--- Desafio3.cs
+++ Desafio3.cs
@@ -22,7 +22,6 @@ public class Program
             Console.WriteLine("Operación completada.");
         }
 
-        // Espera a que el usuario presione Enter para salir
         Console.ReadLine();
     }
 
@@ -33,11 +32,9 @@ public class Program
         Console.WriteLine("Ingrese el segundo número:");
         string input2 = Console.ReadLine();
 
-        // Convertir los valores ingresados a enteros
         int num1 = Convert.ToInt32(input1);
         int num2 = Convert.ToInt32(input2);
 
-        // Realizar la suma
         int suma = num1 + num2;
         return suma;
     }
